5b02df5dd9f276cadc9ea0b09371f7a66cc3fc42
asommar <a.sommar@ya.ru>
2024-07-08 20:05:52 +0300

    refactoring brain-calc

--- src/engine.php
+++ src/engine.php
@@ -2,3 +2,14 @@
 
 // "исполняемый файл" -> "игра" -> "движок".
 namespace PHP\Project\Engine;
+use function cli\line;
+use function cli\prompt;
+
+function greetUser(): string
+{
+    line('Welcome to the Brain Game!');
+    $userName = prompt('May I have your name?');
+    line("Hello, %s!", $userName);
+
+    return $userName;
+}
--- src/games/brain-calc.php
+++ src/games/brain-calc.php
@@ -12,6 +12,7 @@ namespace Php\Project\Games\Brain\Calc;
 
 use function cli\line;
 use function cli\prompt;
+use function PHP\Project\Engine\greetUser;
 
 function getRandomOperator(): string
 {
@@ -36,9 +37,8 @@ function doMath($a, $operation, $b): ?int
 function playBrainCalc(): void
 {
     $countLevels = NUMBER_OF_LEVELS;
-    line('Welcome to the Brain Game!');
-    $userName = prompt('May I have your name?');
-    line("Hello, %s!", $userName);
+    $userName = greetUser();
+
     line('What is the result of the expression?');
     for ($i = 0; $i < $countLevels; $i++) {
         $number1 = rand(1, 50);
@@ -47,13 +47,12 @@ function playBrainCalc(): void
         line("Question: %d%s%d", $number1, $operator, $number2);
         $answer = prompt("Your answer");
         $correctAnswer = doMath($number1, $operator, $number2);
-        if ($answer == $correctAnswer) {
-            line("Correct!");
-        } else {
+        if ($answer != $correctAnswer) {
             line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $correctAnswer);
             line("Let's try again, %s!", $userName);
             return;
         }
+        line("Correct!");
     }
     line("Congratulations, %s", $userName);
 }
