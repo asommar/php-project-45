68f947a889468ed6727f35d211a417b6f223072d
asommar <a.sommar@ya.ru>
2024-07-08 21:05:59 +0300

    refactoring brain-gcd

--- src/engine.php
+++ src/engine.php
@@ -13,3 +13,16 @@ function greetUser(): string
 
     return $userName;
 }
+
+function PlayLevel(string $question, string $correctAnswer, string $userName): bool
+{
+    line("Question: %s", $question);
+    $answer = prompt("Your answer");
+    if ($answer != $correctAnswer) {
+        line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $correctAnswer);
+        line("Let's try again, %s!", $userName);
+        return false;
+    }
+    line("Correct!");
+    return true;
+}
--- src/games/brain-gcd.php
+++ src/games/brain-gcd.php
@@ -5,23 +5,20 @@ namespace Php\Project\Games\Brain\Gcd;
 use function cli\line;
 use function cli\prompt;
 use function PHP\Project\Engine\greetUser;
+use function PHP\Project\Engine\PlayLevel;
 
 function playBrainGcd()
 {
     $userName = greetUser();
-    line('What is the result of the expression?');
+    line('Find the greatest common divisor of given numbers.');
     for ($i = 0; $i < NUMBER_OF_LEVELS; $i++) {
         $number1 = rand(1, 50);
         $number2 = rand(1, 50);
-        line("Question: %d %d", $number1, $number2);
-        $answer = prompt("Your answer");
+        $question = $number1 . ' ' . $number2;
         $correctAnswer = gmp_gcd($number1, $number2);
-        if ($answer != $correctAnswer) {
-            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $correctAnswer);
-            line("Let's try again, %s!", $userName);
+        if (!playLevel($question, $correctAnswer, $userName)) {
             return;
         }
-        line("Correct!");
     }
     line("Congratulations, %s", $userName);
 }
