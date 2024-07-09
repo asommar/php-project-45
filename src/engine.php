2ed4159e2cbfe2eed0bb233ffcd0f919f16fa610
asommar <a.sommar@ya.ru>
2024-07-09 22:17:41 +0300

    fix game name in greeting

--- src/engine.php
+++ src/engine.php
@@ -7,7 +7,7 @@ use function cli\prompt;
 
 function greetUser(): string
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $userName = prompt('May I have your name?');
     line("Hello, %s!", $userName);
 
